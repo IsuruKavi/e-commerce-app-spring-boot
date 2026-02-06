cf0556b8254b7192f0100a0062efb642aad9a02a
Isuru_kavi <isurukavindalakshan863@gmail.com>
2026-02-06 22:07:40 +0530

    setup data base and flyway

--- services/product/src/main/resources/db/migration/V1__init_database.sql
+++ services/product/src/main/resources/db/migration/V1__init_database.sql
@@ -11,7 +11,7 @@ create table if not exists product (
     available_quantity double precision not null,
     price numeric(38,2),
     category_id integer
-         constraint anytextyouwant(fk_product_category) references category
+         constraint anytextyouwant_fk_product_category references category
 );
 
 create sequence if not exists category_seq increment by 50;
